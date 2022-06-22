33807228e32f6aa73821754db133bcdffa449ed3
ElizaZenere <elizazenere@gmail.com>
2022-06-22 16:00:24 -0300

    Maqueta Inicial

---
+++ technicalChallenge/technicalChallenge/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using technicalChallenge.Models;
+
+namespace technicalChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Simplify()
+        {
+            return View();
+        }
+        public IActionResult ValidateName()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
